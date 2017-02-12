corrected indexing error in dot_finder and added distance calculation.  Added sz_dissipation_histogram which is a work in progress.

dot_finder.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def get_bloch_data(file_name):
+def get_bloch_data():
     """
     Opens a bloch.dat file and extracts the time data and s data from it
     file_name is the name of the path to the file / file name
@@ -14,7 +14,7 @@ def get_bloch_data(file_name):
     s_list = []
     
     bloch_file_name = "../sim_results/run12/bloch.dat"
-    bloch_file = open(file_name)
+    bloch_file = open(bloch_file_name)
 
     for line in bloch_file:
         line_list = line.split()
@@ -56,9 +56,9 @@ def find_min_sz():
     for i,s in enumerate(s_list[timestep-1]):
         if (i+1)%3==0:
             if float(s)<s_min[0]:
-                s_min = float(s),(i+1)/3
+                s_min = float(s),(i+1)/3-1
             elif float(s)<s_min2[0] and float(s)>s_min[0]:
-                s_min2 = float(s),(i+1)/3
+                s_min2 = float(s),(i+1)/3-1
                 
     return s_min,s_min2
     
@@ -79,6 +79,17 @@ def find_dot():
             dots.append(position)
     return dots
     
+def calc_dist(dots):
+    dot1 = dots[0]
+    dot2 = dots[1]
+    
+    x_dist = dot1[0] - dot2[0]
+    y_dist = dot1[1] - dot2[1]
+    z_dist = dot1[2] - dot2[2]
+
+    return np.sqrt(x_dist**2+y_dist**2+z_dist**2)
+
+
 def main():
     
     bloch_file_name = "../sim_results/run12/bloch.dat"